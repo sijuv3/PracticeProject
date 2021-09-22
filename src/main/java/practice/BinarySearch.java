23b2636da4731c525a58e40ae0f583c06f144cc6
Siju Varghese <siju.varghese@verizonwireless.com>
2021-09-21 23:39:21 -0500

    code snippet

---
+++ src/main/java/practice/BinarySearch.java
@@ -0,0 +1,26 @@
+package practice;
+
+public class BinarySearch {
+
+    public static void main(String args[]) {
+        BinarySearch bs = new BinarySearch();
+        System.out.println(bs.search(new int[]{-1, 0, 3, 5, 9, 12}, 2));
+    }
+
+    private int search(int[] nums, int target) {
+        if (nums.length <= 0) return -1;
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int midpoint = left + (right - left) / 2;
+            if (nums[midpoint] == target)
+                return midpoint;
+            else if (nums[midpoint] > target)
+                right = midpoint - 1;
+            else
+                left = midpoint + 1;
+        }
+
+        return -1;
+    }
+}
---
+++ src/main/java/practice/MaxConsOnes.java
@@ -0,0 +1,26 @@
+package practice;
+/*
+ * Given a binary array nums, return the maximum number of consecutive 1's in the array.
+ */
+public class MaxConsOnes {
+
+    public static void main(String args[]) {
+        MaxConsOnes m = new MaxConsOnes();
+        System.out.println(m.findMaxConsecutiveOnes(new int[]{1,1,0,1,1,1}));
+    }
+
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int contOnes = 0;
+        int pointer = 0;
+        while (pointer < nums.length) {
+            if (nums[pointer] == 1)
+                contOnes++;
+            else
+                contOnes = 0;
+            max = Math.max(contOnes, max);
+            pointer++;
+        }
+        return max;
+    }
+}
